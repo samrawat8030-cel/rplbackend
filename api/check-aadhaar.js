15ab3598316565d9632803ce56917e230ffd5b86
rud320 <rp0114497@gmail.com>
2026-07-24 17:33:23 +0530

    fix: exopsing check-adhaar endpoint

--- api/check-aadhaar.js
+++ api/check-aadhaar.js
@@ -7,6 +7,10 @@ module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ success: false, message: "Method not allowed" });
   }
